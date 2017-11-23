27012a751efb66b1a3862bdfd5323c37e355f655
Luis Saraiva <up201404302@fe.up.pt>
2017-11-23 09:28:16 +0000

    Update header.php

---
+++ html/templates/common/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>LTW PROJETO TO-DO LISTS</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <header>
+            <h1>Online To-Do Lists</h1>
+            <div id="signup">
+              <a href="register.html">Register</a>
+              <a href="login.html">Login</a>
+              <a href="logout.html">Logout</a>
+            </div>
+        </header>
