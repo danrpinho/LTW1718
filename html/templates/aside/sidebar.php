d8f53645b91ab8a39801300bc0c14656495e4874
danrpinho <up201505302@fe.up.pt>
2017-11-28 12:26:22 +0000

    Tweaking sidebar

--- html/templates/aside/sidebar.php
+++ html/templates/aside/sidebar.php
@@ -1,10 +1,13 @@
 <aside id="sidebar">
     <?php include_once('html/templates/common/user.php'); ?>
 
+    <section id="due">
+
     <h3>These items are almost due!</h3>
     <ul>
         <li>
             <h4>List Title Data Due</h4>
         </li>
     </ul>
+    </section>
 </aside>
