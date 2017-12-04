b078d7b3768dcbacd5c932cb1f6326b9cede2b01
danrpinho <up201505302@fe.up.pt>
2017-12-04 17:14:26 +0000

    There's a small issue with sidebar items not appearing in certain pages

--- user_settings.php
+++ user_settings.php
@@ -2,10 +2,16 @@
     include_once('html/includes/init.php');
     include_once('html/database/connection.php');
     include_once('html/templates/common/header.php');
-    include_once('html/database/list.php');
-    $expiringLists = getExpiringItems();
-    $expiredLists = getExpiringItems();
-    include_once('html/templates/aside/sidebar.php');
-    include_once('html/templates/session/user_settings.php');
+
+    if (isset($_SESSION['username']) && $_SESSION['username'] != '') {
+        include_once('html/database/list.php');
+        $expiringLists = getExpiringItems();
+        $expiredLists = getExpiringItems();
+        include_once('html/templates/aside/sidebar.php');
+        include_once('html/templates/session/user_settings.php');
+    } else {
+        include_once('html/templates/session/login.php');
+    }
+
     include_once('html/templates/common/footer.php');
 ?>
