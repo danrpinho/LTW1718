00d78fcdd2f59f4a45821d765c1c0f71530fb0f1
danrpinho <up201505302@fe.up.pt>
2017-12-07 10:12:02 +0000

    Dealt with merge conflicts again

--- change_email.php
+++ change_email.php
@@ -2,7 +2,6 @@
     include_once('html/includes/init.php');
     include_once('html/database/connection.php');
     include_once('html/templates/common/header.php');
-<<<<<<< HEAD
     if (isset($_SESSION['username']) && $_SESSION['username'] != '') {
         include_once('html/database/list.php');
         $expiringLists = getExpiringItems();
@@ -13,12 +12,4 @@
         include_once('html/templates/session/login.php');
     }
         include_once('html/templates/common/footer.php');
-=======
-    include_once('html/database/list.php');
-    $expiredLists = getExpiredItems();
-    $expiringLists = getExpiringItems();
-    include_once('html/templates/aside/sidebar.php');
-    include_once('html/templates/session/change_email.php');
-    include_once('html/templates/common/footer.php');
->>>>>>> d5f4094a32cc4d75cbd45e3a014c8dbca199f262
 ?>
--- change_pw.php
+++ change_pw.php
@@ -2,7 +2,6 @@
     include_once('html/includes/init.php');
     include_once('html/database/connection.php');
     include_once('html/templates/common/header.php');
-<<<<<<< HEAD
     if (isset($_SESSION['username']) && $_SESSION['username'] != '') {
         include_once('html/database/list.php');
         $expiringLists = getExpiringItems();
@@ -14,12 +13,3 @@
     }
         include_once('html/templates/common/footer.php');
 ?>
-=======
-    include_once('html/database/list.php');
-    $expiredLists = getExpiredItems();
-    $expiringLists = getExpiringItems();
-    include_once('html/templates/aside/sidebar.php');
-    include_once('html/templates/session/change_pw.php');
-    include_once('html/templates/common/footer.php');
-?>
->>>>>>> d5f4094a32cc4d75cbd45e3a014c8dbca199f262
