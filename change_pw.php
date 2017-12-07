d5f4094a32cc4d75cbd45e3a014c8dbca199f262
up201404302 <up201404302@fe.up.pt>
2017-12-07 09:38:06 +0000

    Adicionar Item feito

--- change_pw.php
+++ change_pw.php
@@ -3,8 +3,8 @@
     include_once('html/database/connection.php');
     include_once('html/templates/common/header.php');
     include_once('html/database/list.php');
-    $expiredLists = getExpiringItems();
-    $expiringLists = getExpiredItems();
+    $expiredLists = getExpiredItems();
+    $expiringLists = getExpiringItems();
     include_once('html/templates/aside/sidebar.php');
     include_once('html/templates/session/change_pw.php');
     include_once('html/templates/common/footer.php');
