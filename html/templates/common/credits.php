ef649018966c33a484999e7d3bc516db1e42f6c2
danrpinho <up201505302@fe.up.pt>
2017-12-12 15:46:25 +0000

    Fixed credits omg so cute

--- html/templates/common/credits.php
+++ html/templates/common/credits.php
@@ -1,23 +1,17 @@
 <section class="credits">
     <article id="credits">
         <h3>About</h3>
-        <p class="code">&lt;span id="authors"&gt;</p>
-        <p class="code">&#8291;&lt;p&gt;Group 65:&lt;/p&gt;</p>
-        <p class="code">&#8291;&lt;ul&gt;</p>
-        <p class="code">&#8291;&#8291;&lt;li&gt;</p>
-        <p class="code">&#8291;&#8291;&#8291;&lt;p&gt;Daniel Pinho, 201505302&lt;/p&gt;</p>
-        <p class="code">&#8291;&#8291;&lt;/li&gt;</p>
-        <p class="code">&#8291;&#8291;&lt;li&gt;</p>
-        <p class="code">&#8291;&#8291;&#8291;&lt;p&gt;Francisco Andrade, 201503481&lt;/p&gt;</p>
-        <p class="code">&#8291;&#8291;&lt;/li&gt;</p>
-        <p class="code">&#8291;&#8291;&lt;li&gt;</p>
-        <p class="code">&#8291;&#8291;&#8291;&lt;p&gt;Luís Saraiva, 201404302&lt;/p&gt;</p>
-        <p class="code">&#8291;&#8291;&lt;/li&gt;</p>
-        <p class="code">&#8291;&lt;/ul&gt;</p>
-        <p class="code">&lt;/span&gt;</p>
-        <p class="code">&lt;span id="curricular_unit"&gt;</p>
-        <p class="code">&#8291;&lt;p&gt;LTW, 2017/18&lt;/p&gt;</p>
-        <p class="code">&#8291;&lt;p&gt;MIEIC/FEUP&lt;/p&gt;</p>
-        <p class="code">&lt;/span&gt;</p>
+        <pre>&lt;span id="authors"&gt;</pre>
+        <pre>    &lt;p&gt;Group 65:&lt;/p&gt;</pre>
+        <pre>    &lt;ul&gt;</pre>
+        <pre>        &lt;li&gt;&lt;p&gt;Daniel Pinho, 201505302&lt;/p&gt;&lt;/li&gt;</pre>
+        <pre>        &lt;li&gt;&lt;p&gt;Francisco Andrade, 201503481&lt;/p&gt;&lt;/li&gt;</pre>
+        <pre>        &lt;li&gt;&lt;p&gt;Luís Saraiva, 201404302&lt;/p&gt;&lt;/li&gt;</pre>
+        <pre>    &lt;/ul&gt;</pre>
+        <pre>&lt;/span&gt;</pre>
+        <pre>&lt;span id="curricular_unit"&gt;</pre>
+        <pre>    &lt;p&gt;LTW, 2017/18&lt;/p&gt;</pre>
+        <pre>    &lt;p&gt;MIEIC/FEUP&lt;/p&gt;</pre>
+        <pre>&lt;/span&gt;</pre>
     </article>
 </section>
