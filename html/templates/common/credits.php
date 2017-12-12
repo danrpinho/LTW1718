743cb81c15201691d19539e98d27f3145e03576e
danrpinho <up201505302@fe.up.pt>
2017-12-12 15:41:42 +0000

    Working on credits

--- html/templates/common/credits.php
+++ html/templates/common/credits.php
@@ -1,8 +1,23 @@
 <section class="credits">
     <article id="credits">
         <h3>About</h3>
-        <p>Made with &#9829 by</p>
-        <p class="authors">Daniel Pinho, up201505302 | Francisco Andrade, up2015----- | Luís Saraiva, up2014-----</p>
-        <p>LTW, 2017/18 | MIEIC/FEUP</p>
+        <p class="code">&lt;span id="authors"&gt;</p>
+        <p class="code">&#8291;&lt;p&gt;Group 65:&lt;/p&gt;</p>
+        <p class="code">&#8291;&lt;ul&gt;</p>
+        <p class="code">&#8291;&#8291;&lt;li&gt;</p>
+        <p class="code">&#8291;&#8291;&#8291;&lt;p&gt;Daniel Pinho, 201505302&lt;/p&gt;</p>
+        <p class="code">&#8291;&#8291;&lt;/li&gt;</p>
+        <p class="code">&#8291;&#8291;&lt;li&gt;</p>
+        <p class="code">&#8291;&#8291;&#8291;&lt;p&gt;Francisco Andrade, 201503481&lt;/p&gt;</p>
+        <p class="code">&#8291;&#8291;&lt;/li&gt;</p>
+        <p class="code">&#8291;&#8291;&lt;li&gt;</p>
+        <p class="code">&#8291;&#8291;&#8291;&lt;p&gt;Luís Saraiva, 201404302&lt;/p&gt;</p>
+        <p class="code">&#8291;&#8291;&lt;/li&gt;</p>
+        <p class="code">&#8291;&lt;/ul&gt;</p>
+        <p class="code">&lt;/span&gt;</p>
+        <p class="code">&lt;span id="curricular_unit"&gt;</p>
+        <p class="code">&#8291;&lt;p&gt;LTW, 2017/18&lt;/p&gt;</p>
+        <p class="code">&#8291;&lt;p&gt;MIEIC/FEUP&lt;/p&gt;</p>
+        <p class="code">&lt;/span&gt;</p>
     </article>
 </section>
